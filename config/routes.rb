b505dbc1b2c4e69e4a2a8fdd5ebf9c2dd33a5fa6
Cristiano Souza <ltianosouzal@outlook.com>
2024-12-27 22:08:20 -0300

    add user model and style for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root "home#index"
+  devise_for :users
+
+  authenticate :user, ->(user) { user.admin? } do
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
